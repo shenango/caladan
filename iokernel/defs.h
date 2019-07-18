joshuafried/caladan-ae

iokernel: accept core list as argument

iokernel/defs.h
@@ -265,6 +265,9 @@ extern int cores_init(void);
 extern int control_init(void);
 extern int dp_clients_init();
 
+extern bool allowed_cpus_supplied;
+extern DEFINE_BITMAP(allowed_cpus, NCPU);
+
 /*
  * other dataplane functions
  */

shenango/caladan

iokernel/defs.h
@@ -325,6 +325,9 @@ extern int tx_init(void);
 extern int dp_clients_init(void);
 extern int dpdk_late_init(void);
 
+extern bool allowed_cores_supplied;
+extern DEFINE_BITMAP(input_allowed_cores, NCPU);
+
 /*
  * dataplane RX/TX functions
  */