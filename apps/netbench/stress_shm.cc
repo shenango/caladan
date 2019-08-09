bb41723bd8ab841f64affb1db486c2a2d069029c
Zhenyuan Ruan <zainryan95@gmail.com>
2019-08-09 17:09:48 -0400

    netbench: another stress variant which expose runtime counters through the shared memory.

---
+++ apps/netbench/stress_shm.cc
@@ -0,0 +1,76 @@
+extern "C" {
+#include <base/log.h>
+}
+
+#include "runtime.h"
+#include "sync.h"
+#include "synthetic_worker.h"
+#include "thread.h"
+#include "timer.h"
+
+#include <chrono>
+#include <iostream>
+#include <unistd.h>
+#include <sys/shm.h>
+
+#define SHM_KEY (0x123)
+
+namespace {
+
+int threads;
+uint64_t n;
+std::string worker_spec;
+
+void MainHandler(void *arg) {
+  uint64_t *cnt;
+  int shmid = shmget((key_t)SHM_KEY, sizeof(uint64_t) * threads,
+		 0666 | IPC_CREAT);
+  void *shm = NULL;
+  shm = shmat(shmid, 0, 0);
+  cnt = (uint64_t *)shm;
+	
+  rt::WaitGroup wg(1);
+
+  for (int i = 0; i < threads; ++i) {
+    rt::Spawn([&, i]() {
+      auto *w = SyntheticWorkerFactory(worker_spec);
+      if (w == nullptr) {
+        std::cerr << "Failed to create worker." << std::endl;
+        exit(1);
+      }
+
+      while (true) {
+        w->Work(n);
+        cnt[i]++;
+        rt::Yield();
+      }
+    });
+  }
+
+  // never returns
+  wg.Wait();
+}
+
+}  // anonymous namespace
+
+int main(int argc, char *argv[]) {
+  int ret;
+
+  if (argc != 5) {
+    std::cerr << "usage: [config_file] [#threads] [#n] [worker_spec]"
+              << std::endl;
+    return -EINVAL;
+  }
+
+  threads = std::stoi(argv[2], nullptr, 0);
+  n = std::stoul(argv[3], nullptr, 0);
+  worker_spec = std::string(argv[4]);
+
+  ret = runtime_init(argv[1], MainHandler, NULL);
+  if (ret) {
+    printf("failed to start runtime\n");
+    return ret;
+  }
+
+  return 0;
+}
---
+++ apps/netbench/stress_shm_query.cc
@@ -0,0 +1,51 @@
+extern "C" {
+#include <base/log.h>
+}
+
+#include "runtime.h"
+#include "sync.h"
+#include "synthetic_worker.h"
+#include "thread.h"
+#include "timer.h"
+
+#include <chrono>
+#include <iostream>
+#include <unistd.h>
+#include <sys/shm.h>
+
+#define SHM_KEY (0x123)
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    std::cerr << "usage: [#threads]"
+              << std::endl;
+    return -EINVAL;
+  }
+
+  int threads = std::stoi(argv[1], nullptr, 0);
+	
+  uint64_t *cnt;
+  int shmid = shmget((key_t)SHM_KEY, sizeof(uint64_t) * threads,
+		 0666 | IPC_CREAT);
+  void *shm = NULL;
+  shm = shmat(shmid, 0, 0);
+  cnt = (uint64_t *)shm;
+
+  uint64_t last_total = 0;
+  auto last = std::chrono::steady_clock::now();
+  while (1) {
+    sleep(1);
+    auto now = std::chrono::steady_clock::now();
+    uint64_t total = 0;
+    double duration =
+	    std::chrono::duration_cast<std::chrono::duration<double>>(now - last)
+	    .count();
+    for (int i = 0; i < threads; i++) total += cnt[i];
+    log_info("%lf %lu", static_cast<double>(total - last_total) / duration,
+	     (unsigned long)time(NULL));
+    last_total = total;
+    last = now;	  
+  }
+	
+  return 0;
+}
