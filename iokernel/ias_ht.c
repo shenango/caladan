e317c1be5a4d15232628b93d2eafb534fcf5fd00
Zhenyuan Ruan <zainryan95@gmail.com>
2019-08-26 01:10:56 -0400

    IAS: fix the ipc counter.

--- iokernel/ias_ht.c
+++ iokernel/ias_ht.c
@@ -44,11 +44,13 @@ static void ias_ht_poll_one(struct ias_data *sd, struct thread *th)
 	/* update unpaired IPC metrics */
 	run_us = ((float)cur_tsc - sd->ht_start_running_tsc[core]) /
 		 cycles_per_us;
-	idle_us = ((float)cur_tsc - cores_idle_tsc[sib]) /
-		 cycles_per_us;
-	if (run_us - us < WARMUP_US || idle_us - us < WARMUP_US)
+	if (run_us - us < WARMUP_US)
 		return;
 	if (!cores[sib]) {
+		idle_us = ((float)cur_tsc - cores_idle_tsc[sib]) /
+			cycles_per_us;
+		if (idle_us - us < WARMUP_US)
+			return;
 		ias_ewma(&sd->ht_unpaired_ipc, ipc,
 			 MIN(100.0, us) * IAS_EWMA_FACTOR);
 		return;
