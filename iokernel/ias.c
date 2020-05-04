b50e241908d3f28298053919a7d2090fde21f975
Josh Fried <joshuafried@gmail.com>
2020-05-04 19:30:02 -0400

    ias: add preference for empty cores

--- iokernel/ias.c
+++ iokernel/ias.c
@@ -267,8 +267,12 @@ static float ias_core_score(struct ias_data *sd, unsigned int core,
 	if (bitmap_test(sd->reserved_cores, core))
 		score += 2.0f;
 
-	if (cfg.mutualpair && cores[sched_siblings[core]] != sd)
+	if (cfg.mutualpair && cores[sched_siblings[core]] != sd) {
 		score += 3.0f;
+		if (cores[sched_siblings[core]] == NULL && cores[core] == NULL)
+			score += 2.0f;
+	}
+
 
 	return score;
 }
