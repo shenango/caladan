273b3f7c4c5bbc6511f990a11f48a76ae05515ec
Josh Fried <joshuafried@gmail.com>
2020-08-17 23:14:53 -0400

    bump mlx4 support

--- dpdk.sh
+++ dpdk.sh
@@ -13,8 +13,7 @@ patch -p 1 -d dpdk/ < ixgbe_19_11.patch
 if lspci | grep -q 'ConnectX-[4,5]'; then
    patch -p 1 -d dpdk/ < mlx5_19_11.patch
 elif lspci | grep -q 'ConnectX-3'; then
-    patch -p 1 -d dpdk/ < mlx4_18_11.patch
-    sed -i 's/CONFIG_RTE_LIBRTE_MLX4_PMD=n/CONFIG_RTE_LIBRTE_MLX4_PMD=y/g' dpdk/config/common_base
+    patch -p 1 -d dpdk/ < mlx4_19_11.patch
 fi
 
 # Configure/compile dpdk
