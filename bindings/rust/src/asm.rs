e937589e342be8c9564c01c27a6d5beda997a5ba
Akshay Narayan <akshayn@mit.edu>
2021-10-29 20:00:25 +0000

    llvm_asm -> asm

--- bindings/rust/src/asm.rs
+++ bindings/rust/src/asm.rs
@@ -1,24 +1,23 @@
 #[inline]
 pub fn cpu_relax() {
-    unsafe { llvm_asm!("pause" :::: "volatile") }
+    unsafe { core::arch::x86_64::_mm_pause() }
 }
+
 #[inline]
 pub fn cpu_serialize() {
-    unsafe { llvm_asm!("cpuid" : : : "rax", "rbx", "rcx", "rdx": "volatile") }
+    unsafe {
+        core::arch::x86_64::__cpuid(0);
+    }
 }
+
 #[inline]
 pub fn rdtsc() -> u64 {
-    let a: u32;
-    let d: u32;
-    unsafe { llvm_asm!("rdtsc" : "={eax}"(a), "={edx}"(d) : : : "volatile" ) };
-    (a as u64) | ((d as u64) << 32)
+    unsafe { core::arch::x86_64::_rdtsc() }
 }
+
 #[inline]
 pub fn rdtscp() -> (u64, u32) {
-    let a: u32;
-    let d: u32;
-    let c: u32;
-    unsafe { llvm_asm!("rdtscp" : "={eax}"(a), "={edx}"(d), "={ecx}"(c) : : : "volatile") };
-
-    ((a as u64) | ((d as u64) << 32), c)
+    let mut aux: u32 = 0;
+    let tsc = unsafe { core::arch::x86_64::__rdtscp(&mut aux as *mut u32) };
+    (tsc, aux)
 }
