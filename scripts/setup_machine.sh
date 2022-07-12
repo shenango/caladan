cfcfc1353f8acbd073df1d50251d9c845481e342
Josh Fried <joshuafried@gmail.com>
2022-07-12 00:18:39 -0600

    scripts: remove mistaken update

--- scripts/setup_machine.sh
+++ scripts/setup_machine.sh
@@ -16,7 +16,11 @@ mknod /dev/ksched c 280 0
 chmod uga+rwx /dev/ksched
 
 # reserve huge pages
-echo 5192 > /sys/devices/system/node/node1/hugepages/hugepages-2048kB/nr_hugepages
+echo 5192 > /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages
+echo 0 > /sys/devices/system/node/node1/hugepages/hugepages-2048kB/nr_hugepages
+for n in /sys/devices/system/node/node[2-9]; do
+	echo 0 > $n/hugepages/hugepages-2048kB/nr_hugepages
+done
 
 # load msr module
 modprobe msr
