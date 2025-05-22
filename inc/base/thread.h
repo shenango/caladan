d69ecb5ff9739255a37c00c0249e5d643d2c5efa
Josh Fried <joshuafried@gmail.com>
2025-05-21 22:23:35 -0400

    base: remove brittle optimization for perthread

--- inc/base/thread.h
+++ inc/base/thread.h
@@ -14,7 +14,7 @@
 /* used to define perthread variables */
 #define DEFINE_PERTHREAD(type, name)                                           \
   typeof(type) __perthread_##name __perthread                                  \
-      __attribute__((section(".perthread,\"\",@nobits#")))
+      __attribute__((section(".perthread")))
 
 /* used to make perthread variables externally available */
 #define DECLARE_PERTHREAD(type, name) extern DEFINE_PERTHREAD(type, name)
